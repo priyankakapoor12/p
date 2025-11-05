e648ee36775b7298a2a611e2931ec07b6f9069b3
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-05 13:33:51 +0530

    by me

--- p.sh
+++ p.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 NUMBER1="$1"
 NUMBER2="$2"
+TODAYSDATE="$ date"
 
 SUM=$((NUMBER1+NUMBER2))
 echo "SUM of $NUMBER1 and $NUMBER2 is = $SUM"
 
 SUBTRACTION=$((NUMBER1-NUMBER2))
 echo "Subtraction of $NUMBER1 and $NUMBER2 is = $SUBTRACTION"
+echo "Today's date is $TODAYSDATE"
+
 
 
