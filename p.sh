c7d8f445430283c0768e0143c712b1fd4dc57153
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-05 12:45:36 +0530

    by me

--- p.sh
+++ p.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
-echo "Please enter you username: "
+#!/bn/bash
+# Set correct username and password
+CORRECT_USER="Priyanka"
+CORRECT_PASS="Devops"
+
+echo "Enter Username:"
 read USERNAME
-echo "username entered = $USERNAME"
 
-echo "Please enter you Passwors: "
-read PASSWORD
+echo -n "Enter Password: "
+read -s PASSWORD
 echo
-echo "Password entered = $PASSWORD"
 
+# Username Check
+if [[ "$USERNAME" != "$CORRECT_USER" ]]; then
+    echo "❌ Incorrect Username!"
+    exit 1
+fi
 
+# Password Check
+if [[ "$PASSWORD" != "$CORRECT_PASS" ]]; then
+    echo "❌ Incorrect Password!"
+    exit 1
+fi
 
+echo "✅ Login Successful!"
+echo "Welcome, $USERNAME!"
