c53934b191abd2e1a2ba40568fd12fa52927e132
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-05 14:04:31 +0530

    by me

--- p.sh
+++ p.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-MOVIES=("Pushpa" "Dhoni" "Sachin")
-
-echo "First Movie = ${MOVIES[0]}"
-echo "Second Movie = ${MOVIES[1]}"
-echo "Third Movie = ${MOVIES[2]}"
-echo "All Movies = ${MOVIES[@]}"
+echo "All variables passed : $@"
+echo "Number of variables; $#"
+echo "Script name ; $0"
+echo "Present working directory : $PWD"
+echo "Home directory of the current user; $HOME"
+echo "Which user is running this script ; $USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "Process if last command in background:$!"
